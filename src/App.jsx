4895806b69c827022e52fb6456de97a1f0c499e7
farhan <fk015148@gmail.com>
2021-07-11 19:15:40 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './header'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      {/* header */}
+        <Header/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function header() {
+    return (
+        <>
+          <section id='header'>
+                <img src="images/instagram.png"
+                alt=" instagram_name" className="logo"
+                className='app_headerImage' />
+          </section>  
+        </>
+    )
+}
+
+export default header
